617480b65777794bfef14b8f1d0426c04b15bf9c
Deivide <deivide.ferreirap@gmail.com>
2021-06-06 01:45:24 -0300

    desafio079.py concluído. desafio079b.py e desafio079c.py excluídos.

--- desafios/desafio079.py
+++ desafios/desafio079.py
@@ -1,14 +1,24 @@
+from time import sleep
 list = list()
-while True:
+user = 0
+while user != 'N':
     valor = str(input('Digite um valor: '))
-    if not list.count(valor) == 1:  #Checando se o valor é único.
+    while not valor.isnumeric():
+        valor = str(input('Digite um valor: '))
+    if list.count(valor) == 0:  #Checando se o valor é único.
         list.append(valor)
 
-    user = str(input('Você gostaria de continuar?\033[40m[S/N]\033[m')).strip().upper()[0]
+    user = str(input('Você gostaria de continuar?\033[40m[S/N]\033[m ')).strip().upper()
+    while user != 'S' and user != 'N' or user.isspace() or user == '':  #Checando se foi S ou N.
+        print('\033[31mResposta inválida! Tente novamente!\033[m')
+        sleep(0.1)
+        user = str(input('Você gostaria de continuar?\033[40m[S/N]\033[m ')).strip().upper()
+        if user == 'N':
+            break
     if user == 'N':
         break
 
-print(f'Os valores únicos, em ordem crescente, foram:')
+print('-' * 50, '\nOs valores únicos, em ordem crescente, foram:')
 list.sort()
 for v in list:
     print(f'{v}', end=' ')
