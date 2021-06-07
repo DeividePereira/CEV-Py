90e058fc2a9f928e5191a2b88ba1761255733fa2
Deivide <deivide.ferreirap@gmail.com>
2021-06-07 06:08:08 -0300

    Atualizações Mundo 3.

--- desafios/desafio081.py
+++ desafios/desafio081.py
@@ -1,8 +1,3 @@
-# vários números, coloque-os numa lista
-# A) quantos números foram digitados
-# B) a lista de núm de forma decrescente
-# C) se o valor 5 foi digitado ou está na lista.
-# todos estão com erro
 from random import randint
 list = []
 user = 0
@@ -11,8 +6,8 @@ while user != 'N':
     num = str(input('Digite um número: '))
     while not num.isnumeric():
         num = str(input('Digite um número: '))
-    if list.count(num) == 0:  #Checando se o valor é único.
-        list.append(num)
+    if list.count(int(num)) == 0:  #Checando se o valor é único.
+        list.append(int(num))
 
     user = str(input('Você gostaria de continuar?\033[40m[S/N]\033[m ')).strip().upper()
     while user != 'S' and user != 'N' or user.isspace() or user == '':  #Checando se foi S ou N.
@@ -23,26 +18,19 @@ while user != 'N':
     if user == 'N':
         break
 
-print(f'Foram dados {len(list)} números únicos.')
-print(list)
+print(f'Foram dados {len(list)} números\033[33m únicos\033[m.')
 
-#1 list_rev = list.sort()[::-1]  #TypeError: 'NoneType' object is not subscriptable
-#2 print(f'A lista decrescente é:\n{list_rev}')  #erro: <built-in method reverse of list object at 0x7f46c0e194c0>
-#3 print(f'A lista decrescente é:\n{list.reverse()}')  #erro: "None"
-
-print(list.sort())  #4
-list.sort()  #está dando erro pq está como STR
-print(list[::-1])
+list.sort()
 list_rev = list[::-1]
-print(f'A lista decrescente é:\n{list_rev}')  #erro desconhecido
+print(f'\033[36mA lista decrescente é:\n{list_rev}\033[m')
 
-if list.count('5') == 0:
+if list.count(5) == 0:
     print('O número 5 não apareceu na lista.')
 else:
     print(f'O número 5 apareceu na lista.')
 
-n_random = randint(int(min(list)), int(max(list)))  #Se há um número aleatório entre o mínimo e o máximo da lista.
-if list.count(f'{n_random}') == 0:
-    print(f'O número {n_random} não apareceu na lista.')
+n_random = randint(min(list), max(list))  #Se há um número aleatório entre o mínimo e o máximo da lista.
+if list.count(n_random) == 0:
+    print(f'O número aleatório, entre o maior e menor da lista, {n_random} não apareceu na lista.')
 else:
-    print(f'O número {n_random} apareceu na lista.')
+    print(f'O número aleatório, entre o maior e menor da lista, {n_random} apareceu na lista.')
---
+++ desafios/desafio081b.py
@@ -0,0 +1,48 @@
+# vários números, coloque-os numa lista
+# A) quantos números foram digitados
+# B) a lista de núm de forma decrescente
+# C) se o valor 5 foi digitado ou está na lista.
+# todos estão com erro
+from random import randint
+list = []
+user = 0
+
+while user != 'N':
+    num = str(input('Digite um número: '))
+    while not num.isnumeric():
+        num = str(input('Digite um número: '))
+    if list.count(num) == 0:  #Checando se o valor é único.
+        list.append(num)
+
+    user = str(input('Você gostaria de continuar?\033[40m[S/N]\033[m ')).strip().upper()
+    while user != 'S' and user != 'N' or user.isspace() or user == '':  #Checando se foi S ou N.
+        print('\033[31mResposta inválida! Tente novamente!\033[m')
+        user = str(input('Você gostaria de continuar?\033[40m[S/N]\033[m ')).strip().upper()
+        if user == 'N':
+            break
+    if user == 'N':
+        break
+
+print(f'Foram dados {len(list)} números únicos.')
+print(list)
+
+#1 list_rev = list.sort()[::-1]  #TypeError: 'NoneType' object is not subscriptable
+#2 print(f'A lista decrescente é:\n{list_rev}')  #erro: <built-in method reverse of list object at 0x7f46c0e194c0>
+#3 print(f'A lista decrescente é:\n{list.reverse()}')  #erro: "None"
+
+print(list.sort())  #4
+list.sort()  #está dando erro pq está como STR
+print(list[::-1])
+list_rev = list[::-1]
+print(f'A lista decrescente é:\n{list_rev}')  #erro desconhecido
+
+if list.count('5') == 0:
+    print('O número 5 não apareceu na lista.')
+else:
+    print(f'O número 5 apareceu na lista.')
+
+n_random = randint(int(min(list)), int(max(list)))  #Se há um número aleatório entre o mínimo e o máximo da lista.
+if list.count(f'{n_random}') == 0:
+    print(f'O número {n_random} não apareceu na lista.')
+else:
+    print(f'O número {n_random} apareceu na lista.')
