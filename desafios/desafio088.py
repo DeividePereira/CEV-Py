105f3eb66b7b96fe74da6913a3a556caad6d90d5
Deivide <deivide.ferreirap@gmail.com>
2021-07-02 16:27:50 -0300

    Updates Mundo 3.

--- desafios/desafio088.py
+++ desafios/desafio088.py
@@ -3,26 +3,35 @@ from time import sleep
 nome = 'Mega Sena Ant\'s Harpoon'
 mega_sena = list()
 jogo = list()
+j = 0
 print(f'=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=\n\033[36m{nome:^48}\033[m'
       f'\n=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=')
 user = int(input('Insira quantos jogos de mega sena deseja: '))
-print(f'=-=-=-=-=-=-=- Sorteando {user} jogos -=-=-=-=-=-=-=')
+while user <= 0:
+    print('\033[31mValor Inválido! Tente novamente.\033[m')
+    user = int(input('Insira quantos jogos de mega sena deseja: '))
+print(f'=-=-=-=-=-=-=-\033[36m Sorteando {user} jogos\033[m -=-=-=-=-=-=-=')
 
-'''for a in range(0, user):        #Número de jogos
-    for b in range(0, 6):       #Cada jogo deve ter 6 números.
-        jogo.append((randint(1, 60)))   #Cada número vai de 1 a 60
-        mega_sena.append(jogo.sort()[:])
-    print(f'Jogo n°{a + 1}: {jogo}')
-    sleep(0.25)
-    jogo.clear()'''
+'''           Requisitos:
+ Deve haver 'user' número de jogos;
+ Cada jogo deve haver 6 números;
+ Cada número deve variar de 1 a 60, sem repetir.
+           Planejamento:
+ 'mega_sena' é o agrupamento de todos os jogos;
+ 'jogo' é um dos 'user' grupos de números;
+ 'j' é a contagem de número de jogos;
+ 'num' é cada número de 'jogo';
+ 'n' é a contagem de números de cada 'jogo' '''
 
-for a in range(0, user):        #Número de jogos
-    for b in range(0, 6):
-        num = randint(1, 6)
+while len(mega_sena) != user:
+    while len(jogo) != 6:       #Contagem da quantidade de números.
+        num = randint(1, 60)    #Números de 1 a 60.
         if num not in jogo:
             jogo.append(num)
-            b += 1
-    print(f'Jogo n°{a + 1}: {sorted(jogo)}')
+            jogo.sort()
+    j += 1                      #Contagem do número de jogos
     sleep(0.25)
+    print(f'Jogo n°{j}: {jogo}')
+    mega_sena.append(jogo[:])
     jogo.clear()
-print('Boa sorte!')
+print(f'=-=-=-=-=-=-=-=-=-\033[36m Boa sorte!\033[m -=-=-=-=-=-=-=-=-=')
