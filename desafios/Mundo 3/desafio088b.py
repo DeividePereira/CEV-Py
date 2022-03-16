fe2a852c7af779cfa22a0faccce955b1268797ec
Deivide <80660377+DeividePereira@users.noreply.github.com>
2022-03-16 06:30:52 -0300

    Update Mundo 3
    desafio099.py concluído.

--- desafios/Mundo 3/desafio088b.py
+++ desafios/Mundo 3/desafio088b.py
@@ -10,7 +10,7 @@ print(f'=-=-=-=-=-=-=- Sorteando {user} jogos -=-=-=-=-=-=-=')
 
 for a in range(0, user):        #Número de jogos
     for b in range(0, 6):       #Cada jogo deve ter 6 números.
-        jogo.append((randint(0, 60)))
+        jogo.append(randint(0, 60))
     print(f'Jogo n°{a + 1}: {sorted(jogo)}')
     sleep(0.25)
     jogo.clear()
--- desafios/Mundo 3/desafio099.py
+++ desafios/Mundo 3/desafio099.py
@@ -1,2 +1,30 @@
-# Função maior(): receba vários valores int. Analise e diga qual é o maior.
-# (Não precisa ser input)
+from random import randint
+from time import sleep
+lista = list()
+def ranking(n):
+    print('-' * 50)
+    print(f' Sorteando {n} números:')
+    for n in range(0, n):
+        m = randint(1,100)
+        lista.append(m)
+
+    for z in lista:
+        print(f'{z}', end=' ', flush=True)
+        sleep(0.1)
+    print()
+
+    lista.sort()
+    lista.reverse()
+    print('-' * 25, '\n   Ranking:')
+    for z2 in lista:
+        print(f'{z2}', end=' ', flush=True)
+        sleep(0.15)
+    print()
+    lista.clear()
+    
+
+ranking(randint(1,30))
+ranking(randint(1,30))
+ranking(randint(1,30))
+print('-' * 50, '\nEncerrando...')
+sleep(0.2)
